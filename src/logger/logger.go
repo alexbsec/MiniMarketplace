53540fa8ce0757779c01867bb4e0b371e258ae82
Joao Buschinelli <j.a.c.buschinelli@gmail.com>
2025-02-04 11:28:41 -0500

    changed name and test passed

--- src/logger/logger.go
+++ src/logger/logger.go
@@ -1,4 +1,4 @@
-package logger
+package outlogger
 
 import (
 	"bytes"
@@ -18,7 +18,7 @@ const (
 	yellow    = 33
 	blue      = 34
 	lightGray = 37
-    darkGray  = 90
+	darkGray  = 90
 )
 
 const (
@@ -41,11 +41,11 @@ func (h *Handler) Enabled(ctx context.Context, level slog.Level) bool {
 	return h.handle.Enabled(ctx, level)
 }
 
-func (h *Handler) WithAttrs(attrs []slog.Attr) *Handler {
+func (h *Handler) WithAttrs(attrs []slog.Attr) slog.Handler {
 	return &Handler{handle: h.handle.WithAttrs(attrs), bytes: h.bytes, mutex: h.mutex}
 }
 
-func (h *Handler) WithGroup(name string) *Handler {
+func (h *Handler) WithGroup(name string) slog.Handler {
 	return &Handler{handle: h.handle.WithGroup(name), bytes: h.bytes, mutex: h.mutex}
 }
 
@@ -69,39 +69,39 @@ func (h *Handler) Handle(ctx context.Context, record slog.Record) error {
 		level = colorize(red, level)
 	}
 
-    attrs, err := h.computeAttrs(ctx, record)
-    if err != nil {
-        return err
-    }
+	attrs, err := h.computeAttrs(ctx, record)
+	if err != nil {
+		return err
+	}
 
-    bytes, err := json.MarshalIndent(attrs, "", " ")
-    if err != nil {
-        return err
-    }
+	bytes, err := json.MarshalIndent(attrs, "", " ")
+	if err != nil {
+		return err
+	}
 
 	fmt.Println(
-        colorize(lightGray, record.Time.Format(timeFormat)),
-        level, 
-        colorize(color, record.Message),
-        colorize(darkGray, string(bytes)),
-    )
+		colorize(lightGray, record.Time.Format(timeFormat)),
+		level,
+		colorize(color, record.Message),
+		colorize(darkGray, string(bytes)),
+	)
 	return nil
 }
 
 func NewHandler(opts *slog.HandlerOptions) *Handler {
-    if opts == nil {
-        opts = &slog.HandlerOptions{}
-    }
-    b := &bytes.Buffer{}
-    return &Handler{
-        handle: slog.NewJSONHandler(b, &slog.HandlerOptions{
-            Level:  opts.Level,
-            AddSource: opts.AddSource,
-            ReplaceAttr: suppressDefaults(opts.ReplaceAttr),
-        }),
-        bytes: b,
-        mutex: &sync.Mutex{},
-    }
+	if opts == nil {
+		opts = &slog.HandlerOptions{}
+	}
+	b := &bytes.Buffer{}
+	return &Handler{
+		handle: slog.NewJSONHandler(b, &slog.HandlerOptions{
+			Level:       opts.Level,
+			AddSource:   opts.AddSource,
+			ReplaceAttr: suppressDefaults(opts.ReplaceAttr),
+		}),
+		bytes: b,
+		mutex: &sync.Mutex{},
+	}
 }
 
 func (h *Handler) computeAttrs(ctx context.Context, record slog.Record) (map[string]any, error) {
@@ -125,17 +125,15 @@ func (h *Handler) computeAttrs(ctx context.Context, record slog.Record) (map[str
 }
 
 func suppressDefaults(next func([]string, slog.Attr) slog.Attr) func([]string, slog.Attr) slog.Attr {
-    return func(groups []string, a slog.Attr) slog.Attr {
-        if a.Key == slog.TimeKey ||
-        a.Key == slog.LevelKey ||
-        a.Key == slog.MessageKey {
-            return slog.Attr{}
-        }
-        if next == nil {
-            return a
-        }
-        return next(groups, a)
-    }
+	return func(groups []string, a slog.Attr) slog.Attr {
+		if a.Key == slog.TimeKey ||
+			a.Key == slog.LevelKey ||
+			a.Key == slog.MessageKey {
+			return slog.Attr{}
+		}
+		if next == nil {
+			return a
+		}
+		return next(groups, a)
+	}
 }
-
-
--- src/main.go
+++ src/main.go
@@ -1,9 +1,28 @@
 package main
 
 import (
-    "github.com/alexbsec/MiniMarketplace/logger"
+	"errors"
+	"fmt"
+	"log/slog"
+
+	"github.com/alexbsec/MiniMarketplace/src/outlogger"
 )
 
 func main() {
-    fmt.Println("Servidor iniciado!")
+	logger := slog.New(
+		outlogger.NewHandler(nil).WithAttrs(
+			[]slog.Attr{
+				slog.Group("status",
+					slog.String("key1", "test"),
+					slog.String("key2", "test2"),
+				),
+			},
+		),
+	)
+
+	// Log an error with additional context
+	logger.Error("Ops, test failed successfully!",
+		slog.String("error", errors.New("bad luck").Error()),
+	)
+	fmt.Println("Servidor iniciado!")
 }
